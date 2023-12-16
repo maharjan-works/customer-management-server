7ba615646a705629daac08cfc7b02f3364e9d41d
maharjan.works <maharjan.works@gmail.com>
2023-12-16 17:18:33 -0500

    pilot project + db connection done

---
+++ src/test/java/com/maharjanoworks/customermanagementserver/CustomerManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maharjanoworks.customermanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerManagementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
