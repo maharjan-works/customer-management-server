0c6ea9dddaa8ee318ec04901425c3ecea0fd06b8
maharjan.works <maharjan.works@gmail.com>
2023-12-16 22:22:22 -0500

    @CrossOrgin added in CustomerController

--- src/main/java/com/maharjanoworks/customermanagementserver/controller/CustomerController.java
+++ src/main/java/com/maharjanoworks/customermanagementserver/controller/CustomerController.java
@@ -3,12 +3,14 @@ package com.maharjanoworks.customermanagementserver.controller;
 import com.maharjanoworks.customermanagementserver.model.Customer;
 import com.maharjanoworks.customermanagementserver.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins="http://localhost:4200/")
 @RestController
 @RequestMapping("/api/v1/")
 public class CustomerController {
